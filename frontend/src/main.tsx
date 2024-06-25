ad0d26c45c61454325ba820208357d5d727f45af
RSelwa <selwa.raphael@gmail.com>
2024-06-25 13:49:51 +0200

    🚑 Remove radix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
 
 ReactDOM.createRoot(document.getElementById("root") as Element).render(
   <React.StrictMode>
-    <Theme>
-      <App />
-    </Theme>
+    <App />
   </React.StrictMode>
 );
