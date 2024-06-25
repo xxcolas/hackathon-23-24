e91e02f6efa17d6561b0c5e8bd66fcafa38483f0
RSelwa <selwa.raphael@gmail.com>
2024-06-25 12:27:10 +0200

    💄 table priority

--- frontend/src/pages/table.tsx
+++ frontend/src/pages/table.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import Table from "@/components/table";
+import { users } from "@/mocks";
 
 const TablePage = () => {
-  const data = [
-    { name: "John", age: 28, city: "New York" },
-    { name: "Jane", age: 22, city: "San Francisco" },
-    { name: "Mike", age: 32, city: "Chicago" },
-  ];
   return (
     <div>
-      <Table data={data} />
+      <Table data={users} />
     </div>
   );
 };
