3808c11497cb82f9cb24a6c2aa33e88af000e0e6
Nicolas CAPELLA <colas.capel73@gmail.com>
2024-06-25 12:26:00 +0200

    chore: Update gptController to use user-provided message in getChatResponse

--- backend/controllers/gptController.js
+++ backend/controllers/gptController.js
@@ -1,7 +1,9 @@
 import getChatResponse from "../services/mistralService.js";
 
 const gptController = async (req, res) => {
-  const chatResponse = await getChatResponse();
+  // const { userMessage } = req.body;
+  const userMessage = "Je saigne énormement j'ai besoin d'aide. Tout était parfait sauf qu'on ne m'a pas averti que 3 gouttes de pipi ne suffisait pas à ma sortie : j'ai dû revenir le lendemain et me faire poser une sonde urinaire ! Il y a une erreur : cette opération a été annulée au profit de la pose d'un patch à 15h demain. Quelques gouttes sur la cicatrice mais d après l infirmières il n y a pas d infection.";
+  const chatResponse = await getChatResponse(userMessage);
   try {
     return res.status(200).json({ message: chatResponse });
   } catch (error) {
--- backend/services/mistralService.js
+++ backend/services/mistralService.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: ".env.local", override: true });
 
-const getChatResponse = async () => {
+const getChatResponse = async (userMessage) => {
   const client = new MistralClient(process.env.MISTRAL_API_KEY);
   const chatResponse = await client.chat({
     // model: "open-mixtral-8x7b",
@@ -21,7 +21,7 @@ const getChatResponse = async () => {
           "Je veux un résumé simple de ce texte sous forme de tirets. " +
           "Je veux également que tu me donnes un ordre de priorité (ex: low, medium, high). " +
           "Je veux absolument cette forme d'objet JSON et rien d'autres: '{ 'description': '- Problème de santé, - Urine partiel', 'priority': 'low' }'." +
-          "Voici le texte que tu vas devoir traiter: Je saigne énormement j'ai besoin d'aide. Tout était parfait sauf qu'on ne m'a pas averti que 3 gouttes de pipi ne suffisait pas à ma sortie : j'ai dû revenir le lendemain et me faire poser une sonde urinaire ! Il y a une erreur : cette opération a été annulée au profit de la pose d'un patch à 15h demain. Quelques gouttes sur la cicatrice mais d après l infirmières il n y a pas d infection.",
+          "Voici le texte que tu vas devoir traiter: " + userMessage,
       },
     ],
   });
