9c42013f7782f9e141a52e61571318f10a3a7fe7
Nicolas CAPELLA <colas.capel73@gmail.com>
2024-06-27 15:22:45 +0200

    Added toast after sent audio

--- frontend/src/pages/client.tsx
+++ frontend/src/pages/client.tsx
@@ -16,6 +16,7 @@ const Client = () => {
   const [blobURL, setBlobURL] = useState(null)
   const [text, setText] = useState("")
   const [transcript, setTranscript] = useState("")
+  const [toast, setToast] = useState(false)
 
   const onStop = (recordedBlob) => {
     setBlobURL(recordedBlob.blob)
@@ -28,6 +29,15 @@ const Client = () => {
   }
 
   const sendAudio = async (blob: Blob, transcript: string) => {
+    setToast(true)
+    setTimeout(() => {
+      setToast(false)
+    }, 3000)
+
+    setAudio(null)
+    setTranscript("")
+    setText("")
+
     const userId = searchParams.get("uid")
     const file = new File([blob], "file")
     await uploadAudioFileWithTranscript(userId, file, transcript)
@@ -105,6 +115,24 @@ const Client = () => {
           </div>
         </div>
       )}
+      {
+        toast && (
+          <div className="absolute top-10 right-10 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg" role="alert">
+            <div className="flex p-4">
+              <div className="flex-shrink-0">
+                <svg className="flex-shrink-0 size-4 text-teal-500 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+                </svg>
+              </div>
+              <div className="ms-3">
+                <p className="text-sm text-gray-700">
+                  Audio envoyé avec succès
+                </p>
+              </div>
+            </div>
+          </div>
+        )
+      }
     </div>
   )
 }
